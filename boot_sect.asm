09af53f5df0872776bc404e4f6abbb26db9ae795
Andrea Stocco <astocco11@gmail.com>
2022-08-18 17:21:48 +0200

    aggiunto semplice bootloader

---
+++ boot_sect.asm
@@ -0,0 +1,28 @@
+[org 0x7c00] ; il BIOS carica il bootloader sempre all'indirizzo 0x7c00. di conseguenze e' necessario aggiungere questo offset ad ogni indirizzo
+             ; per poter reperire la cella di memoria desiderata. questa istruzione dice al compilatore di aggiungere tale offset ad ogni indirizzo
+             ; di memoria utilizzato, senza doverlo aggiungere manualmente ogni volta.
+
+bits 16
+
+main:
+  mov bx, msg
+  call print_string
+  jmp $
+
+print_string:
+  pusha
+  mov ah, 0x0e
+.loop:
+  mov al, [bx]
+  int 0x10 ; stampo il carattere in al
+  add bx, 1
+  mov cx, [bx]
+  cmp cx, 0
+  jne .loop
+  popa
+  ret
+
+msg db 'Booting OS', 0
+
+times 510 - ($ - $$) db 0
+dw 0xaa55
